130ebdcd732fac7cff602604c04eb466612b6d2d
Anastasia Zatylnikova <azatylnikova@gmail.com>
2024-10-09 23:36:20 +0600

    add webpack-dev-server react

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counter from "./components/Counter";
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<Counter />);

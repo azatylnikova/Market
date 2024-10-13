8e451a75073417fc59aa291e9b8b15811df17975
Anastasia Zatylnikova <azatylnikova@gmail.com>
2024-10-14 02:46:35 +0600

    add module css splitting Lazy Suspence

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import './Counter.scss';
+import * as classes from './Counter.module.scss';
+
+
 
 const Counter = () => {
     const [count, setCount] = useState(0);
@@ -7,9 +9,9 @@ const Counter = () => {
     const increment = () => {
         setCount(count + 1);
     }
-
+    console.log(classes, 'rr');
     return (
-        <div>
+        <div className={classes.btn}>
             <h1>{count}</h1>
             <button onClick={increment}>increment</button>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Counter from "./components/Counter";
-import { render } from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 
-render(
-                <Counter />,
-    document.getElementById('root'),);
+const root = createRoot(document.getElementById('root'));
+root.render(  <BrowserRouter>
+    <App />
+</BrowserRouter>,);

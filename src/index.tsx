e58cd4338474eb485bb1530b291a453f5d9cea4d
Anastasia Zatylnikova <azatylnikova@gmail.com>
2024-10-20 23:35:13 +0600

    add SvgLoader AppRouter LileLoader FSD

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client'; // Измените импорт
+import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 
+const container = document.getElementById('root'); // Ваш контейнер
+const root = ReactDOM.createRoot(container); // Создание корня
 
-const root = createRoot(document.getElementById('root'));
-root.render(  <BrowserRouter>
-    <App />
-</BrowserRouter>,);
+root.render(
+    <BrowserRouter>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>
+)
+
